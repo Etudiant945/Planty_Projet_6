1c8942a9045bb7429571855e96fc8e7f05fca3a1
Alexis Regnon <regnon.alexis94@gmail.com>
2024-08-24 18:21:09 +0200

    personnalisation du footer

--- wp-content/themes/blankslate-child/footer.php
+++ wp-content/themes/blankslate-child/footer.php
@@ -1,1 +1,14 @@
 </main>
+<?php get_sidebar(); ?>
+</div>
+<footer class="pied">
+        <div>
+            <a href="<?php echo esc_url(home_url('/mentions-legales')); ?>" class="pied-nav">Mentions légales</a>
+        </div>
+    </footer>
+</body>
+</html>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
